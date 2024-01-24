1ec08c93294a22039ca0f18ce3b9d3fdf987f0a3
Igor Anikeev <anikeev@softlab.ru>
2024-01-24 18:58:07 +0400

    Правки

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,29 +1,22 @@
 package web.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
-import web.service.CarServiceImpl;
-
-import java.util.ArrayList;
-import java.util.List;
+import web.service.CarService;
 
 @Controller
 public class CarController {
-    private final CarServiceImpl carService;
+    private final CarService carService;
 
-    public CarController(CarServiceImpl carService) {
+    public CarController(CarService carService) {
         this.carService = carService;
     }
 
     @GetMapping(value = "/cars")
     public String viewCars(@RequestParam(defaultValue = "5") int count, ModelMap model) {
-
-        List<Car> cars = carService.getCarsList(count);
-        model.addAttribute("cars", cars);
+        model.addAttribute("cars", carService.getCarsList(count));
         return "cars";
     }
 }
